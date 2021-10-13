90062ca2e03fc4ac7047bfc05fa70f9a11d79ac6
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2021-10-14 00:02:31 +0200

    Made if statements to buy gold, wood and metal farmers

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,9 @@ public class Main {
 
         while (true) {
             System.out.println();
-            System.out.print("Write what you wish to do.\nTo show the amount of wood, gold or metal you have,your income, and your army, write status." +
+            System.out.print("Write what you wish to do.\nTo show the amount of wood, gold or metal you have, your income, and your army, write status." +
                     "\nTo show the cost of knights, write knight. \nTo show the cost of sorcerers, write sorcerer." +
-                    "\nTo show the cost of more wood, gold and metal farmers, write farmers. Each farmer increase your income by 1.");
+                    "\nTo show the cost of more wood, gold and metal farmers, write farmers. Each farmer increases your income by 1. ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             //Type 0 to break loop
@@ -57,12 +57,12 @@ public class Main {
                 System.out.println("You have " + army.getKnight() + " knights. Type buyknight to buy a knight for 20 Gold, 10 Wood and 5 Metal.");
             }
             if (input.equals("buyknight")) {
-                army.setKnight(army.getKnight()+1);
-                System.out.println(army.getKnight());
+                army.setKnight(army.getKnight() + 1);
+                System.out.println("You now have " + army.getKnight() + ".");
             }
             if (input.equals("buysorcerer")) {
-                army.setSorcerer(army.getSorcerer()+1);
-                System.out.println(army.getSorcerer());
+                army.setSorcerer(army.getSorcerer() + 1);
+                System.out.println("You now have " + army.getSorcerer() + ".");
             }
             if (input.equals("sorcerer")) {
                 System.out.println("              _,-'|\n" +
@@ -92,6 +92,36 @@ public class Main {
                 System.out.println("You have " + resurser.getWoodFarmer() + " wood farmers. Type buywoodfarmer to buy a wood farmer for 10 gold and 10 wood.");
                 System.out.println("You have " + resurser.getGoldFarmer() + " gold farmers. Type buygoldfarmer to buy a gold farmer for 20 gold.");
                 System.out.println("You have " + resurser.getMetalFarmer() + " metal farmers. Type buymetalfarmer to buy a metal farmer for 30 gold, 10 wood and 5 metal.");
+            }
+            if (input.equals("buywoodfarmer")) {
+                if (resurser.getGold() >= 10 && resurser.getWood() >= 10) {
+                    resurser.setWoodFarmer(resurser.getWoodFarmer() + 1);
+                    resurser.setWood(resurser.getWood() - 10);
+                    resurser.setGold(resurser.getGold() - 10);
+                    System.out.println("Your wood income has increased to " + resurser.getWoodFarmer() + ".");
+                    resurser.wood();
+                } else
+                    System.out.println("You do not have enough resources to buy a wood farmer.");
+            }
+            if (input.equals("buygoldfarmer")) {
+                if (resurser.getGold() >= 20) {
+                    resurser.setGoldFarmer(resurser.getGoldFarmer() + 1);
+                    resurser.setGold(resurser.getGold() - 20);
+                    System.out.println("Your gold income has increased to " + resurser.getWoodFarmer() + ".");
+                    resurser.gold();
+                } else
+                    System.out.println("You do not have enough resources to buy a gold farmer.");
+            }
+            if (input.equals("buymetalfarmer")) {
+                if (resurser.getGold() >= 30 && resurser.getWood() >= 10 && resurser.getMetal() >= 5) {
+                    resurser.setMetalFarmer(resurser.getMetalFarmer() + 1);
+                    resurser.setGold(resurser.getGold() - 30);
+                    resurser.setWood(resurser.getWood() - 10);
+                    resurser.setMetal(resurser.getMetal() - 5);
+                    System.out.println("Your metal income has increased to " + resurser.getMetalFarmer() + ".");
+                    resurser.metal();
+                } else
+                    System.out.println("You do not have enough resources to buy a metal farmer.");
             }
         }
 
